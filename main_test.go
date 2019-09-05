02f9ecf573615cda13a45fe11b4df3e6c31d8474
Wizkaley <kaleyeshan@gmail.com>
2019-09-05 11:58:26 +0530

    Tested with Coverage

--- main_test.go
+++ main_test.go
@@ -4,11 +4,14 @@ package main
 import (
 	"github.ibm.com/dash/dash_utils/dashtest"
 	"testing"
+	"time"
 )
 
 
 
+
 func TestMain(m *testing.M){
-	main()
+	go main()
 	dashtest.ControlCoverage(m)
+	time.Sleep(1*time.Second)
 }
